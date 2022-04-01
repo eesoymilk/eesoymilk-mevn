cef061e7f071cbee33a46e1d0b4ae878dafe6811
qiyoudaoyi <qiyoudaoyi@gmail.com>
2022-04-02 01:48:13 +0800

    gitignore

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is Wilson's personal website.
 
 ## Installation
 
-Use the package, either npm or yarn, to install the node modules needed.
+Use a package manager, npm or yarn, to install the node modules needed in **BOTH `.\client` AND `.\server` FOLDER**.
 
 ```shell
 npm install
@@ -27,7 +27,7 @@ VITE_VUE_APP_AUTH0_DOMAIN=dev-t00jyl8z.us.auth0.com
 VITE_VUE_APP_AUTH0_CLIENT_ID=TiIGHWqVIqGQnkNlmppCOcoTGrfskJvy
 VITE_VUE_APP_AUTH0_AUDIENCE=https://eesoymilk-mevn-api.com
 VITE_VUE_APP_AUTH0_CALLBACK_URL=http://localhost:4040
-VITE_VUE_APP_API_SERVER_URL=http://localhost:8080
+VITE_VUE_APP_API_SERVER_URL=https://eesoymilk-mevn.df.r.appspot.com
 ```
 
 Create `.env.production` file.
@@ -46,10 +46,10 @@ Create `.env` file.
 
 ```=
 PORT=8080
-CLIENT_ORIGIN_URL=https://eesoymilk-mevn.web.app
-AUTH0_AUDIENCE=https://eesoymilk-mevn-api.com
-AUTH0_DOMAIN=dev-t00jyl8z.us.auth0.com
-GOOGLE_PROJECT_ID=98809358238
+CLIENT_ORIGIN_URL=
+AUTH0_AUDIENCE=
+AUTH0_DOMAIN=
+GOOGLE_PROJECT_ID=
 ```
 
 ## Development
@@ -83,11 +83,25 @@ I have to apologize to all interviewers viewing my homework. Since this is a Vue
 3. I prefer using a framework I am familiar with, rather than rush to React, to better showcase my web development skills.
 4. I do want to further improve these functionalities done in the homework so making them an integration to my website helps.
 
+And since it's an integration to my original website. The base url for this homework is `https://eesoymilk-mevn.web.app/dcard/frontend`.
+
 #### Client View
 
 > [source code:`/client/src/views/DcardFrontendView.vue`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/client/src/views/DcardFrontendView.vue)
 
 From a client's perspective, I intended to make this whole project fairly simple; that is, a clean UI.<br />So rather than type in the url directly, you can just search the user and their repo page will show up.
+
+#### User's Repos View
+
+> [source code:`/client/src/views/ReposView.vue`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/client/src/views/ReposView.vue)
+
+I didn't put much except for the ones that are instructed. One extra thing I add is the profile picture for the user.
+
+#### Repo View
+
+> [source code:`/client/src/views/RepoView.vue`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/client/src/views/RepoView.vue)
+
+This one is also built as instructed.
 
 #### API Calls
 
@@ -95,7 +109,95 @@ From a client's perspective, I intended to make this whole project fairly simple
 
 I use fetch api to manipulate all API calls. To further simplify things, I made it a class with static functions that do all the jobs for me and also separeated in a `.ts` file.
 
+#### How to Start This App
+
+Go to `./client` folder and start the Vite app on port `4040`.
+
+```shell
+yarn dev
+```
+
+Then, go to `http://localhost:4040/`.
+
+#### How to Interact with This App
+
+You can start your own front-end and go to `http://localhost:4040/`.
+Better yet, you can just visit my website https://eesoymilk-mevn.web.app/dcard/frontend.
+
+Then, you can type in username and search, or you can directly type in the username after the base url. For example, if you want to see my repos, go to this link: https://eesoymilk-mevn.web.app/dcard/frontend/users/qiyoudaoyi/repos
+
 ### Backend
+
+#### Disclaimer
+
+I have to apologize to all interviewers viewing my homework. Since it's not a stand-alone project but an integration to my original website. The base url for each of the api endpoint is
+
+`https://eesoymilk-mevn.df.r.appspot.com/api/shortUrl`
+
+#### DB and Server Hosting
+
+I use **MongoDB Atlas** for my database and connecting it with all the codes I uploaded on **Google Cloud App Engine**.
+
+#### Third-party Dependencies I Used
+
+- Express
+  To make life simplier so that I don't need to touch the original `http` module.
+- Mongoose
+  Manipulating with my DB through a more readable library.
+- shortid
+  Generate shortened url for incoming requests.
+- dotenv
+  Avoid hard-coded information on the source code.
+- Google Cloud Secret Manager
+  Protect people from accessing my DB.
+- JEST
+  JS testing library.
+- supertest
+  Make testing http requests easier.
+
+#### How I built it
+
+This homework is a simple CRUD implementation and some testings. To make the project more organized. I've written in total 5 `.ts` files relating this homework. Let's go through them one by one.
+
+- [`app.ts`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/server/src/app.ts), and [`index.ts`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/server/src/index.ts)
+  Express and middlewares setup, trvial stuff.
+- [`./models/shortUrl.ts`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/server/src/models/shortUrl.ts)
+  Defining schemas for short url.
+- [`./routes/api/shortUrl.ts`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/server/src/routes/api/shortUrl.ts`)
+  Implementation of each of the CURD operation,
+- [`./routes/api/shortUrl.test.ts`](https://github.com/qiyoudaoyi/eesoymilk-mevn/blob/main/server/src/routes/api/shortUrl.test.ts`)
+  Unit-testing using JEST and supertest. Let me be honest here, I do not have prior experience of writing testing for an app. This is my first time doing it.
+
+#### Constrains and Error Handlings
+
+- You cannot shorten url that is already shortened.
+- You cannot shorten url that cannot be access.
+- Respond with status 404 if someone tries to access non-existing short url.
+
+#### Handling Multiple Requests
+
+Since I'm using async/await, most of the problems have been solved for me. More about it on JavaScript callstack topic.
+
+#### How to Start This App
+
+You can't basically, unless you are me. Because to start the password to my DB is held by Google Cloud Secret Manager, and only my google account can access them. However, I built a frontend app where you can interact with my api through my endpoint. You can go to the link below to play with it.
+
+https://eesoymilk-mevn.web.app/dcard/backend
+
+#### How to Interact with This App
+
+Like I said, you can go to the link directly. But, if you prefer the traditional `crul`, you can also do it as demonstrate below.
+
+```shell
+# Upload URL API
+> curl -X POST -H "Content-Type:application/json" https://eesoymilk-mevn.df.r.appspot.com/api/shortUrl -d '{
+"url": "<original_url>",
+"expireAt": "<Date Object>"
+}'
+
+# Redirect URL API
+> curl -L -X GET http://localhost/<url_id> => REDIRECT to original URL
+```
 
 ## License
 
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,7 +2,38 @@
   <div style="min-height: 9vw; margin-top: 5vw">
     <v-row style="width: 100%">
       <v-col cols="5"></v-col>
-      <v-col cols="7"></v-col>
+      <v-col cols="6">
+        <v-card class="hidden-sm-and-down">
+          <v-card-text>
+            <v-row align="center">
+              <v-col cols="2" md="6" align="center">
+                <v-btn
+                  v-for="item in contact"
+                  :key="item.site"
+                  class="mx-1"
+                  :icon="'mdi-' + item.site"
+                  :href="item.link"
+                  variant="plain"
+                  target="_blank"
+                ></v-btn>
+              </v-col>
+              <v-col cols="10" md="6" align="left">
+                <div><v-icon icon="mdi-phone" />(+886)912-045-890</div>
+                <div><v-icon icon="mdi-email" />qiyoudaoyi@gmail.com</div>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+        <v-chip
+          v-for="thing in whatIDos"
+          :key="thing.name"
+          :color="thing.color"
+          size="x-large"
+          class="ma-2 hidden-sm-and-down"
+          >{{ thing.name }}</v-chip
+        >
+      </v-col>
+      <v-col cols="1"></v-col>
     </v-row>
   </div>
   <div style="position: relative">
@@ -18,37 +49,25 @@
     <v-img :src="divider" />
     <v-row style="width: 100%; position: absolute; top: 0px; bottom: 0px">
       <v-col cols="5" align="center"> </v-col>
-      <v-col cols="6" class="align-center justify-center" style="margin: 2vw">
-        <div class="text-subtitle text-lg-h5" v-text="selfIntro"></div>
-        <div
-          class="hidden-lg-and-up"
-          style="
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            height: 100%;
-          "
-        >
-          <div
-            class="text-h5"
-            style="margin: auto auto; flex-basis: 20%; text-align: center"
+      <v-col
+        cols="6"
+        class="align-center justify-center"
+        style="margin: 2vw; margin-top: 4vw"
+      >
+        <div style="margin: auto; flex-basis: 80%">
+          <v-chip
+            v-for="thing in whatIDos"
+            :key="thing.name"
+            :color="thing.color"
+            class="ma-2 hidden-md-and-up"
+            size="large"
+            style="display: block; text-align: center; height: 100%"
+            >{{ thing.name }}</v-chip
           >
-            I
-          </div>
-          <div style="margin: auto; flex-basis: 80%">
-            <v-chip
-              v-for="thing in whatIDos"
-              :key="thing.name"
-              :color="thing.color"
-              class=""
-              style="display: block"
-              v-text="thing.name"
-            ></v-chip>
-          </div>
         </div>
         <iframe
-          class="hidden-md-and-down"
-          src="https://www.youtube.com/embed/MzCb49dcR2E"
+          class="hidden-sm-and-down"
+          :src="iFrameSrc"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -59,33 +78,100 @@
       <v-col cols="1"></v-col>
     </v-row>
   </div>
-  <iframe
-    class="hidden-lg-and-up"
-    src="https://www.youtube.com/embed/MzCb49dcR2E"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen
-    style="display: block; margin: auto; width: 80vw; height: 45vw"
-  ></iframe>
-  <v-divider></v-divider>
-  <v-row>
-    <v-col cols="1" class="hidden-md-and-down"></v-col>
-    <v-col
-      cols="6"
-      md="4"
-      lg="2"
-      v-for="preview in portfolioPreviews"
-      :key="preview.name"
-    >
-      <PortfolioPreviewSheet v-bind="preview" />
-    </v-col>
-    <v-col cols="1" class="hidden-md-and-down"></v-col>
-  </v-row>
+  <v-card class="hidden-md-and-up">
+    <v-card-text>
+      <v-row align="center">
+        <v-col cols="4" align="center">
+          <v-btn
+            v-for="item in contact"
+            :key="item.site"
+            class="mx-1"
+            :icon="'mdi-' + item.site"
+            :href="item.link"
+            variant="plain"
+            target="_blank"
+          ></v-btn>
+        </v-col>
+        <v-col cols="8" align="left">
+          <div><v-icon icon="mdi-phone" />(+886)912-045-890</div>
+          <div><v-icon icon="mdi-email" />qiyoudaoyi@gmail.com</div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+  <v-expansion-panels v-model="panel" variant="accordion" multiple>
+    <v-expansion-panel>
+      <v-expansion-panel-title class="hidden-md-and-up">
+        <v-icon icon="mdi-youtube" size="large" />
+        <div class="text-h6 text-md-h4 text-center">Introduction Video</div>
+        <v-spacer></v-spacer>
+        <div class="text-subtitle" v-if="panel?.includes(0)">Close</div>
+        <div class="text-subtitle" v-else>Expand</div>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text class="hidden-md-and-up">
+        <iframe
+          :src="iFrameSrc"
+          class="hidden-md-and-up"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          style="display: block; margin: 4vw auto; width: 80vw; height: 45vw"
+        ></iframe>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        <v-icon icon="mdi-file-account" size="large" />
+        <div class="text-h6 text-md-h4 text-center">My resume</div>
+        <v-spacer></v-spacer>
+        <div class="text-subtitle" v-if="panel?.includes(1)">Close</div>
+        <div class="text-subtitle" v-else>Expand</div>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-btn
+          style="display: block; margin: 1vw auto"
+          size="large"
+          icon="mdi-google-drive"
+          class="text-body-1 text-md-h6 text-center"
+          :href="resumeLink"
+          target="_blank"
+        ></v-btn>
+        <v-img :src="resume" style="width: 50vw; margin: auto" />
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        <v-icon icon="mdi-book-open-page-variant" size="large" />
+        <div class="text-h6 text-md-h4 text-center">Portfolio Preview</div>
+        <v-spacer></v-spacer>
+        <div class="text-subtitle" v-if="panel?.includes(2)">Close</div>
+        <div class="text-subtitle" v-else>Expand</div>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-row>
+          <v-col cols="1" class="hidden-md-and-down"></v-col>
+          <v-col
+            cols="6"
+            sm="4"
+            md="3"
+            lg="2"
+            v-for="preview in portfolioPreviews"
+            :key="preview.name"
+          >
+            <PortfolioPreviewSheet v-bind="preview" />
+          </v-col>
+          <v-col cols="1" class="hidden-md-and-down"></v-col>
+        </v-row>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script lang="ts" setup>
 import PortfolioPreviewSheet from "@/components/portfolio/PortfolioPreviewSheet.vue";
+// import VuePdfEmbed from "vue-pdf-embed";
+// import resumeSrc from "@/resumePdfSrc";
 
 import profilePic from "@/assets/lumi/ProfilePic.png";
 import divider from "@/assets/lumi/AboutMe3_3.png";
@@ -96,13 +182,15 @@ import frame4 from "@/assets/lumi/frame4.png";
 import frame5 from "@/assets/lumi/frame5.png";
 import frame6 from "@/assets/lumi/frame6.png";
 
+import resume from "@/assets/resume.jpg";
 import soymilk from "@/assets/soymilk-blue-square.jpg";
 import jsCourse from "@/assets/js-course-square.jpg";
 import cyberPunk from "@/assets/cyber-punk-2069-square.jpg";
 import flappyBird from "@/assets/flappy-bird-square.jpg";
 import gameOfBalance from "@/assets/game-of-balance-square.jpg";
-import { useRouter } from "vue-router";
+import { ref, type Ref } from "vue";
 
+const panel = ref([0, 1, 2]) as Ref<number[] | undefined>;
 const portfolioPreviews = [
   {
     imgSrc: soymilk,
@@ -131,19 +219,35 @@ const portfolioPreviews = [
   },
 ];
 
-const selfIntro = "I am Wilson,";
 const whatIDos = [
   {
     name: "learn",
-    color: "amber",
+    color: "orange",
   },
   {
     name: "make",
-    color: "light-green",
+    color: "green",
   },
   {
     name: "innovate",
-    color: "primary",
+    color: "blue",
   },
 ];
+const contact = [
+  {
+    site: "youtube",
+    link: "https://www.youtube.com/channel/UConEV8T9VvkiWWDepM-bNow",
+  },
+  {
+    site: "facebook",
+    link: "https://www.facebook.com/wilson.chang.tw/",
+  },
+  {
+    site: "github",
+    link: "https://github.com/qiyoudaoyi",
+  },
+];
+const iFrameSrc = "https://www.youtube.com/embed/4_OUGf1rxPg";
+const resumeLink =
+  "https://drive.google.com/file/d/1P3qjhBZDrdXtQdGz6A7SP9uFLqbXlzQQ/view?usp=sharing";
 </script>
